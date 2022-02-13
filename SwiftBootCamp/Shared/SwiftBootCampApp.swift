dbd63d347037c85e76a4fc808c2d221fe7e9d154
gondolin24 <gondolin24@gmail.com>
2022-02-13 14:30:13 -0500

    initial commit

---
+++ SwiftBootCamp/Shared/SwiftBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBootCampApp.swift
+//  Shared
+//
+//  Created by Eduardo Guzman Diaz on 2022-02-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
